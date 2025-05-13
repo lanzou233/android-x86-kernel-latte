4eb3d31dc5738209dddbb3270482ccb4c0bbc62e
Qs315490 <qs315490@qq.com>
2025-05-13 17:39:59 +0800

    初始化

---
+++ build_kernels.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ ! -d /home/runner/work ]; then NTHREADS=$(nproc); else NTHREADS=$(($(nproc)*4)); fi
+
+if [ -z "$1" ]; then
+kernels=$(ls -d ./kernels/* | sed 's#./kernels/##g')
+else
+kernels="$1"
+fi
+
+for kernel in $kernels; do
+	echo "Building kernel $kernel"
+	KCONFIG_NOTIMESTAMP=1 KBUILD_BUILD_TIMESTAMP='' KBUILD_BUILD_USER=chronos KBUILD_BUILD_HOST=localhost make -C "./kernels/$kernel" -j"$NTHREADS" O=out || { echo "Kernel build failed"; exit 1; }
+	rm -f "./kernels/$kernel/out/source"
+	if [ -f /persist/keys/MOK.priv ] && [ -f /persist/keys/MOK.pem ]; then
+		echo "Signing kernel $kernel"
+		mv "./kernels/$kernel/out/arch/x86/boot/bzImage" "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" || { echo "Kernel signing failed"; exit 1; }
+		sbsign --key /persist/keys/MOK.priv --cert /persist/keys/MOK.pem "./kernels/$kernel/out/arch/x86/boot/bzImage.unsigned" --output "./kernels/$kernel/out/arch/x86/boot/bzImage" || { echo "Kernel signing failed"; exit 1; }
+	fi
+	echo "Including kernel $kernel headers"
+	srctree="./kernels/$kernel"
+	objtree="./kernels/$kernel/out"
+	SRCARCH="x86"
+	KCONFIG_CONFIG="$objtree/.config"
+	destdir="$srctree/headers"
+	mkdir -p "${destdir}"
+	(
+		cd "${srctree}"
+		echo Makefile
+		find "arch/${SRCARCH}" -maxdepth 1 -name 'Makefile*'
+		find include scripts -type f -o -type l
+		find "arch/${SRCARCH}" -name Kbuild.platforms -o -name Platform
+		find "arch/${SRCARCH}" -name include -o -name scripts -type d
+	) | tar -c -f - -C "${srctree}" -T - | tar -xf - -C "${destdir}"
+	{
+		cd "${objtree}"
+		if grep -q "^CONFIG_OBJTOOL=y" include/config/auto.conf; then
+			echo tools/objtool/objtool
+		fi
+		find "arch/${SRCARCH}/include" Module.symvers include scripts -type f
+		if grep -q "^CONFIG_GCC_PLUGINS=y" include/config/auto.conf; then
+			find scripts/gcc-plugins -name '*.so'
+		fi
+	} | tar -c -f - -C "${objtree}" -T - | tar -xf - -C "${destdir}"
+	cp "${KCONFIG_CONFIG}" "${destdir}/.config"
+done
+
---
+++ prepare_kernels.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+NORMAL_COLOR='\e[0m'
+BLUE_COLOR='\e[1;34m'
+GREEN_COLOR='\e[1;32m'
+RED_COLOR="\e[1;31m"
+
+apply_patches()
+{
+for patch in ./kernel-patches/"$1"/*.patch; do
+	echo -e "${BLUE_COLOR}Applying patch: $patch$NORMAL_COLOR"
+	patch -d"./kernels/$1" -p1 --no-backup-if-mismatch -N < "$patch" || { echo -e "${RED_COLOR}Kernel $1 patch failed$NORMAL_COLOR"; exit 1; }
+done
+echo -e "${GREEN_COLOR}Applying patch all done!$NORMAL_COLOR"
+}
+
+make_config()
+{
+echo "Creating $2 config for kernel $1"
+cp ./kernel-patches/mipad2_defconfig ./kernels/$1/arch/x86/configs/mipad2_defconfig || { echo -e "${RED_COLOR}Kernel $1 configuration failed$NORMAL_COLOR"; exit 1; }
+make -C ./kernels/$1 O=out mipad2_defconfig || { echo -e "${RED_COLOR}Kernel $1 configuration failed$NORMAL_COLOR"; exit 1; }
+}
+
+download_and_patch_kernels()
+{
+for kernel in $kernels; do
+	kernel_remote_path=${kernel_info["$kernel,url"]}
+	kernel_remote_branch=${kernel_info["$kernel,branch"]}
+	echo -e "${BLUE_COLOR}kernel_remote_path=$kernel_remote_path$NORMAL_COLOR"
+
+	git clone --depth=1 $kernel_remote_path -b $kernel_remote_branch --recursive ./kernels/$kernel || { echo -e "${RED_COLOR}Download kernel $kernel failed!$NORMAL_COLOR"; exit 1; }
+
+	apply_patches "$kernel"
+	make_config "$kernel"
+done
+}
+
+rm -rf kernels
+
+GITHUB_URL=https://github.com
+
+declare -A kernel_info=(
+	["6.12,url"]=$GITHUB_URL/android-generic/kernel-zenith
+	["6.12,branch"]="6.12"
+)
+
+kernels="6.12"
+download_and_patch_kernels
+
